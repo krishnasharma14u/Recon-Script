b616472e4a48937f0ee5aec45e275c93e9465bcc
krishnasharma14u <krishnasharma14u@gmail.com>
2021-02-08 20:09:02 +0530

    Update Recon-phase-1.sh

--- Recon-phase-1.sh
+++ Recon-phase-1.sh
@@ -13,15 +13,15 @@ cat subdomains.txt | grep -E "$var1" | tee final-domains.txt && fndo=$(wc -l fin
 
 echo "Resolving Domains"
 
-cat final-domains.txt | massdns -r /usr/share/massdns/lists/resolvers.txt -t A -o S -w resolved-domains.txt && rsdo=$(wc -l resolved-domains.txt | cut -d' ' -f1); echo "total $rsdo domains found" | notify
+cat final-domains.txt | massdns -r /usr/share/massdns/lists/resolvers.txt -t A -o S -w resolved-domains.txt && rsdo=$(wc -l resolved-domains.txt | cut -d' ' -f1); echo "total $rsdo live domains found" | notify
 cut -d " " -f1 resolved-domains.txt | sed 's/.$//' | tee live-domains.txt
 cat live-domains | naabu -json | tee naabu-ports.json; echo "Naabu Port Scan Completed"
 cat live-domains.txt | httpx -title -status-code | tee live-sites.txt
 cat ports.json | grep 8080 | cut -d'"' -f4 | httpx -title -status-code -ports 8080 | tee -a live-sites.txt
 cat ports.json | grep 8443 | cut -d'"' -f4 | httpx -title -status-code -ports 8443 | tee -a live-sites.txt
-cat live-sites.txt | cut -d' ' -f1 | tee ffuf-res.txt 
+cat live-sites.txt | cut -d' ' -f1 | tee ffuf-res.txt && ffres=$(wc -l ffuf-res.txt | cut -d' ' -f1); echo "total $ffres live websites found" | notify
 cat resolved-domains.txt | cut -d'A' -f2 | sed '/\.$/d' | sed -r 's/\s+//g' | sort -u | tee ip.txt
-masscan -iL ip.txt -p0-65535 --rate 10000 -oJ masscan.json &
+masscan -iL ip.txt -p0-65535 --rate 10000 -oJ masscan.json; echo "Masscan completed" &
 mkdir jsfscan && cp ffuf-res.txt jsfscan/ && jsfscan -l ffuf-res.txt -s -e -r &
-ffuf -u FUZZDOMAINS/FUZZ -w domains.txt:FUZZDOMAINS -w  /usr/share/SecLists/Discovery/Web-Content:FUZZ -mc 200 -fl 0,1 -fr 'Not Found','Unathoriza','Forbiden' -t 10000 -of html -o ffuf-report.html &
-cat ffuf-res.txt | nuclei -t cves -t exposed-panels -t fuzzing -t misconfiguration -t vulnerabilities -t default-logins -t exposed-tokens -t helpers -t takeovers -t workflows -t dns -t exposures -t miscellaneous -t technologies -o nucli-result.txt | notify &
+ffuf -u FUZZDOMAINS/FUZZ -w domains.txt:FUZZDOMAINS -w  /usr/share/SecLists/Discovery/Web-Content:FUZZ -mc 200 -fl 0,1 -fr 'Not Found','Unathoriza','Forbiden' -t 10000 -of html -o ffuf-report.html; echo "Fffuf Completed" &
+cat ffuf-res.txt | nuclei -t cves -t exposed-panels -t fuzzing -t misconfiguration -t vulnerabilities -t default-logins -t exposed-tokens -t helpers -t takeovers -t workflows -t dns -t exposures -t miscellaneous -t technologies -o nucli-result.txt; echo "Nuclei Completed" | notify &
